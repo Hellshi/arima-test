568a88bcf9e750996091a93ce1a5444ec20227f3
Hellshi <hcslimaa@gmail.com>
2024-11-20 18:27:38 -0300

    first commit

---
+++ analysis.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+data = pd.read_csv("data.csv", delimiter=";", na_values=["", " "])
+
+numerical_columns = [
+    "Temp. Ins. (C)", "Temp. Max. (C)", "Temp. Min. (C)",
+    "Umi. Ins. (%)", "Umi. Max. (%)", "Umi. Min. (%)",
+    "Pto Orvalho Ins. (C)", "Pto Orvalho Max. (C)", "Pto Orvalho Min. (C)",
+    "Pressao Ins. (hPa)", "Pressao Max. (hPa)", "Pressao Min. (hPa)",
+    "Vel. Vento (m/s)", "Dir. Vento (m/s)", "Raj. Vento (m/s)", "Radiacao (KJ/m²)", "Chuva (mm)"
+]
+
+# Convert columns to numeric, setting errors='coerce' to handle invalid values
+for col in numerical_columns:
+    data[col] = pd.to_numeric(data[col], errors="coerce")
+    if data[col].empty:
+        data[col] = None
+
+print(data.head(20))
+
+data["datetime"] = pd.to_datetime(data["Data"] + " " + data["Hora (UTC)"], format="%d/%m/%Y %H%M")
+
+""" data.drop(columns=["Data", "Hora (UTC)"], inplace=True)
+
+data.fillna(method="ffill", inplace=True)
+
+data.set_index("datetime", inplace=True)
+
+relevant_data = data[["Temp. Ins. (C)", "Chuva (mm)"]] """
