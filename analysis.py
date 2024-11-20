4727a8179a7707b9754384eea9b906209c8447fb
Hellshi <hcslimaa@gmail.com>
2024-11-20 19:30:41 -0300

    fix: hour

--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-data = pd.read_csv("data.csv", delimiter=";", na_values=["", " "])
+data = pd.read_csv("data.csv", delimiter=";", na_values=["", " "], dtype={"Hora (UTC)": str})
+
 
 numerical_columns = [
     "Temp. Ins. (C)", "Temp. Max. (C)", "Temp. Min. (C)",
@@ -10,17 +11,22 @@ numerical_columns = [
     "Vel. Vento (m/s)", "Dir. Vento (m/s)", "Raj. Vento (m/s)", "Radiacao (KJ/m²)", "Chuva (mm)"
 ]
 
+data["Hora (UTC)"] = data["Hora (UTC)"].apply(lambda x: f"{x[:2]}:{x[2:]}")
+
+
 # Convert columns to numeric, setting errors='coerce' to handle invalid values
 for col in numerical_columns:
     data[col] = pd.to_numeric(data[col], errors="coerce")
+
     if data[col].empty:
         data[col] = None
 
-print(data.head(20))
+print(data.head())
 
-data["datetime"] = pd.to_datetime(data["Data"] + " " + data["Hora (UTC)"], format="%d/%m/%Y %H%M")
 
-""" data.drop(columns=["Data", "Hora (UTC)"], inplace=True)
+""" data["datetime"] = pd.to_datetime(data["Data"] + " " + data["Hora (UTC)"], format="%d/%m/%Y %H%M")
+
+data.drop(columns=["Data", "Hora (UTC)"], inplace=True)
 
 data.fillna(method="ffill", inplace=True)
 
