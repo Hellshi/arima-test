83e8b0bd1114ba6bf9603780aced1db0fca62c46
Hellshi <hcslimaa@gmail.com>
2024-11-21 21:01:53 -0300

    feat: adds prediction

--- analysis.py
+++ analysis.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from pmdarima import auto_arima
+
 
 data = pd.read_csv("data.csv", delimiter=";", na_values=["", " "], dtype={"Hora (UTC)": str})
+data["Temp. Ins. (C)"] = data["Temp. Ins. (C)"].astype(str).str.replace(",", ".")
+data["Vel. Vento (m/s)"] = data["Vel. Vento (m/s)"].astype(str).str.replace(",", ".")
+data["Dir. Vento (m/s)"] = data["Dir. Vento (m/s)"].astype(str).str.replace(",", ".")
+data["Raj. Vento (m/s)"] = data["Raj. Vento (m/s)"].astype(str).str.replace(",", ".")
+data["Radiacao (KJ/m²)"] = data["Radiacao (KJ/m²)"].astype(str).str.replace(",", ".")
 
 
 numerical_columns = [
@@ -11,20 +19,19 @@ numerical_columns = [
     "Vel. Vento (m/s)", "Dir. Vento (m/s)", "Raj. Vento (m/s)", "Radiacao (KJ/m²)", "Chuva (mm)"
 ]
 
+print(data.head(50))
+
 data["Hora (UTC)"] = data["Hora (UTC)"].apply(lambda x: f"{x[:2]}:{x[2:]}")
 
 
-# Convert columns to numeric, setting errors='coerce' to handle invalid values
 for col in numerical_columns:
     data[col] = pd.to_numeric(data[col], errors="coerce")
 
     if data[col].empty:
         data[col] = None
 
-print(data.head())
-
-
-""" data["datetime"] = pd.to_datetime(data["Data"] + " " + data["Hora (UTC)"], format="%d/%m/%Y %H%M")
+data["datetime"] = pd.to_datetime(data["Data"] + " " + data["Hora (UTC)"], format="%d/%m/%Y %H:%M")
+print(data.head(20))
 
 data.drop(columns=["Data", "Hora (UTC)"], inplace=True)
 
@@ -32,4 +39,27 @@ data.fillna(method="ffill", inplace=True)
 
 data.set_index("datetime", inplace=True)
 
-relevant_data = data[["Temp. Ins. (C)", "Chuva (mm)"]] """
+relevant_data = data[["Temp. Ins. (C)", "Radiacao (KJ/m²)"]]
+
+relevant_data.plot(subplots=True, figsize=(10, 6), title="Weather Data Trends")
+
+#plt.savefig('grafico.png')
+
+temperature = data["Temp. Ins. (C)"]
+
+temperature = temperature.dropna()
+
+model = auto_arima(temperature, seasonal=False, m=24, trace=True, stepwise=True)
+model.summary()
+
+forecast = model.predict(n_periods=24)
+
+plt.plot(temperature, label="Historical Data")
+plt.plot(pd.date_range(temperature.index[-1], periods=24, freq="H"), forecast, label="Forecast")
+plt.legend()
+plt.savefig("arima-result.png")
+
+forecast = model.predict(n_periods=1)
+
+print(f"A previsão de temperatura para o dia seguinte é: {forecast[0]:.2f}°C")
+
